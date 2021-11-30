afbb3d5f29c2378a8c27ce0831752422937f128f
vyom12 <87473710+vyom12@users.noreply.github.com>
2021-11-30 14:15:37 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,55 @@
+function next(){
+    location.href="./next1.html"
+}
+function next2(){
+    location.href="./next2.html"
+}
+function home2(){
+    location.href="./index.html"
+}
+function donate(){
+    location.href="https://www.wfp.org/support-us/stories/donate?utm_source=google&utm_medium=cpc&utm_campaign=12704016913&utm_content=119837386719&gclid=Cj0KCQjwt-6LBhDlARIsAIPRQcLnU6QnmcAeTd2IYKX-Q5Xaf1M0BaNt7jrNmCFFqB1J8th2_8pGvbcaAp6wEALw_wcB&gclsrc=aw.ds"
+}
+function home(){
+    location.href="./index.html"
+}
+function home2(){
+    location.href="./index.html"
+}
+function next2(){
+    location.href="./next2.html"
+}
+function back(){
+    location.href="./index.html"
+}
+function back2(){
+    location.href="./next1.html"
+}
+function next3(){
+    location.href="./next3.html"
+}
+function home3(){
+    location.href="./index.html"
+}
+function home4(){
+    location.href="./index.html"
+}
+function next4(){
+    location.href="./next4.html"
+}
+function back3(){
+    location.href="./next2.html"
+}
+function back4(){
+    location.href="./next3.html"
+}
+function home5(){
+    location.href="./index.html"
+}
+function donate2(){
+    location.href="https://donate.savethechildren.org/en/donate/child-hunger?utm_source=google&utm_medium=cpc&utm_campaign=gsn%20east%20generic%20hunger%20other&utm_term=stop%20hunger%20charity&gclid=Cj0KCQjwlOmLBhCHARIsAGiJg7kkvDfOlFSP-H0jaCGw4BwUDjmPNt_arenTJDqJfUgzYVwKLbsaq3oaAoqHEALw_wcB"
+}
+function solv(){
+    location.href="https://forms.gle/euUHtpT9uC66b6366"
+}
+    
